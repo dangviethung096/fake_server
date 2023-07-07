609c9e07d325cd105a7164834be4ad0532cc931f
vitcon <dangviethung096@gmail.com>
2023-07-07 15:38:14 +0700

    update build file

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ if [ -z "$VERSION" ]
 then
     echo 'docker build -t dangviethung096/fake_server:latest -f dockerfile .'
     docker build -t dangviethung096/fake_server -f dockerfile .
-    # docker push dangviethung096/fake_server
+    docker push dangviethung096/fake_server
 else
     echo 'docker build -t dangviethung096/fake_server:$VERSION -f dockerfile .'
     docker build -t dangviethung096/fake_server:$VERSION -f dockerfile .
-    # docker push dangviethung096/fake_server:$VERSION
+    docker push dangviethung096/fake_server:$VERSION
 fi
