9a07a441b60b7602560bb055cfadcdf833144f89
vitcon <dangviethung096@gmail.com>
2023-07-07 16:00:21 +0700

    update run

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker pull dangviethung096/fake_server
-docker run -p 10015:10015 --name fake -d --mount type=bind,source=/home/hungdv39gec/data,target=/app/data dangviethung096/fake_server
+docker run -p 10015:10015 --name fake -d --mount type=bind,source=/data,target=/app/data dangviethung096/fake_server
