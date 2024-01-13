f74a42677dec7811f81cb4ba690a56453e7df4d9
vitkon <dangviethung096@gmail.com>
2024-01-13 18:23:04 +0700

    update build in fake server

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-echo 'docker build -t fake_server:latest -f dockerfile .'
-docker build -t fake_server:latest -f dockerfile .
+echo 'docker build -t fake_server:latest -f account_service/dockerfile .'
+docker build -t dangviethung096/fake_server:latest -f account_service/dockerfile .
+
+echo 'docker build -t fake_web:latest -f fake_web/dockerfile .'
+docker build -t dangviethung096/fake_web:latest -f fake_web/dockerfile .
+
+echo 'docker push dangviethung096/fake_web:latest'
+docker push dangviethung096/fake_web:latest
+
+echo 'docker pull dangviethung096/fake_web:latest'
+docker pull dangviethung096/fake_web:latest
--- fake_web/main.go
+++ fake_web/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fake_web/route"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", route.HomePage)
+	fmt.Println("Server running on port 80")
 	http.ListenAndServe(":80", nil)
 }
